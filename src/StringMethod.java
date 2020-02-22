75ea0d579bca797a2539f509e1e267565c5daeda
yourUsername <yourEmail@domain.com>
2020-02-22 14:40:24 -0500

    gg

---
+++ src/StringMethod.java
@@ -0,0 +1,25 @@
+public class StringMethod {
+
+public static void main(String[]args) {
+
+     String name = "Billy Bob Joe";
+
+     System.out.println( "Name: "+ name );
+
+     int age = 26;
+
+     System.out.println( "Age: " + 26);
+     System.out.println( "Uppercase: " + name.toUpperCase() );
+     System.out.println( "lowercase: "+ name.toLowerCase() );
+     System.out.println( "First char: "+ name.charAt(1) );
+     System.out.println( "length: " + name.length());
+     System.out.println( "Last char: "+ name.charAt( 12 ) );
+     System.out.println( "Substring: "+ name.substring( 2, 13 ) );
+
+
+
+}
+
+
+
+}
